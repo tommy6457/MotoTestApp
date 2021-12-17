6c2032d043cf05b02eea644120318ec33df73feb
tommy <tommy5566123@gmail.com>
2021-12-17 17:10:31 +0800

    Initial Commit

---
+++ MotoTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotoTestApp
+//
+//  Created by 蔡尚諺 on 2021/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
